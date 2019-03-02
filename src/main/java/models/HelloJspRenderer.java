33f6b2964eb788627cbe349fb44e864b6ae5cd7e
Artur Semikov <fowlart1988@gmail.com>
2019-03-02 10:50:41 +0200

    Merge branch 'master' of D:\GIT\task05_Lambdas_Streams with conflicts.

--- src/main/java/models/HelloJspRenderer.java
+++ src/main/java/models/HelloJspRenderer.java
@@ -5,14 +5,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class HelloJspRenderer {
-
     private static List<String> entries = new LinkedList<String>();
+    private String code;
 
-    private String tablecode;
-
-    public String getTablecode() {
-        tablecode = getTableCode();
-        return tablecode;
+    public String getCode() {
+        code = getTableCode();
+        return code;
     }
 
     public void addItemEntry(String good_name, String good_price){
