cd9c4f9a671fa029dcc64cf9ae94ee7f9684272f
Semikov_Artur <semikov_artur@epam.com>
2019-03-19 23:55:40 +0200

    minor fixes

--- src/main/java/speech/Speech.java
+++ src/main/java/speech/Speech.java
@@ -10,120 +10,89 @@ import java.util.Arrays;
 import java.util.List;
 
 
-public class Speech
-{
+public class Speech {
 
-	//Create a Synthesizer instance
-	private SynthesiserV2 synthesizer = new SynthesiserV2("AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw");
+    //Create a Synthesizer instance
+    private SynthesiserV2 synthesizer = new SynthesiserV2("AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw");
 
 
-	public void speak(String text)
-	{
-		System.out.println("speaking>> " + text);
-		//Create a JLayer instance
-		synthesizer.setSpeed(1);
-		AdvancedPlayer player = null;
-		try
-		{
-			player = new AdvancedPlayer(synthesizer.getMP3Data(text));
-		}
-		catch (JavaLayerException e)
-		{
-			e.printStackTrace();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-		try
-		{
-			player.play();
-		}
-		catch (JavaLayerException e)
-		{
-			e.printStackTrace();
-		}
-	}
+    public void setLanguage(String code) {
+        synthesizer.setLanguage(code);
+    }
 
-	public void writeToFile(String folder_path, WordTranslate word)
-	{
-		Boolean rez = false;
-		int try_count = 0;
+    public void speak(String text) {
+        System.out.println("speaking>> " + text);
+        //Create a JLayer instance
+        synthesizer.setSpeed(1);
+        AdvancedPlayer player = null;
+        try {
+            player = new AdvancedPlayer(synthesizer.getMP3Data(text));
+        } catch (JavaLayerException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            player.play();
+        } catch (JavaLayerException e) {
+            e.printStackTrace();
+        }
+    }
 
-		while (!rez && (try_count < 5))
-		{
-			try_count++;
-			rez = processWord(folder_path, word.getEngword());
-		}
-		try_count = 0;
-		rez = false;
+    public void writeToFile(String folder_path, WordTranslate word) {
+        Boolean rez = false;
+        int try_count = 0;
 
-		while (!rez && (try_count < 5))
-		{
-			try_count++;
-			rez = processUkrWord(folder_path, word);
-		}
-	}
+        while (!rez && (try_count < 5)) {
+            try_count++;
+            rez = processWord(folder_path, word.getEngword());
+        }
+        try_count = 0;
+        rez = false;
 
-	private boolean processWord(String folder_path, String word)
-	{
+        while (!rez && (try_count < 5)) {
+            try_count++;
+            rez = processUkrWord(folder_path, word);
+        }
+    }
 
-		List<String> word_list = Arrays.asList(word.split(" "));
-		String output_file = new String(word + ".mp3");
-		FileOutputStream outputStream = null;
-		try
-		{
-			outputStream = new FileOutputStream(new File(folder_path + "//" + output_file));
+    private boolean processWord(String folder_path, String word) {
+        this.synthesizer.setLanguage("en-us");
+        String output_file = word + ".mp3";
+        try {
+            process(folder_path, word, output_file);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 
-			synthesizer.setSpeed(1);
-			InputStream inputStream = synthesizer.getMP3Data(word_list);
-			int rez = 0;
-			while (rez > -1)
-			{
-				rez = inputStream.read();
-				outputStream.write(rez);
-			}
-            System.out.println("file " + output_file +  " was created");
-			outputStream.flush();
-			outputStream.close();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-			return false;
-		}
+    private void process(String folder_path, String word, String output_file) throws IOException {
+        List<String> word_list = Arrays.asList(word.split(" "));
+        FileOutputStream outputStream = new FileOutputStream(new File(folder_path + "//" + output_file));
+        synthesizer.setSpeed(1);
+        InputStream inputStream = synthesizer.getMP3Data(word_list);
+        int rez = 0;
+        while (rez > -1) {
+            rez = inputStream.read();
+            outputStream.write(rez);
+        }
+        System.out.println("file " + output_file + " was created");
+        outputStream.flush();
+        outputStream.close();
+    }
 
-		return true;
-	}
 
-	private boolean processUkrWord(String folder_path, WordTranslate wordTranslate)
-	{
-		try
-		{
-			List<String> word_list = Arrays.asList(wordTranslate.getUkrword().split(" "));
-			String output_file = new String(wordTranslate.getEngword() + "_ukr" + ".mp3");
-			FileOutputStream outputStream = new FileOutputStream(new File(folder_path + "//" + output_file));
-			synthesizer.setSpeed(1);
-			InputStream inputStream = synthesizer.getMP3Data(word_list);
-			int rez = 0;
-			while (rez > -1)
-			{
-				rez = inputStream.read();
-				if (rez == -1)
-					break;
-				outputStream.write(rez);
-			}
-			System.out.println("file " + output_file +  " was created");
-			outputStream.flush();
-
-			outputStream.close();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-			return false;
-		}
-
-		return true;
-	}
+    private boolean processUkrWord(String folder_path, WordTranslate wordTranslate) {
+        this.synthesizer.setLanguage("uk");
+        try {
+            String output_file = new String(wordTranslate.getEngword() + "_ukr" + ".mp3");
+            process(folder_path, wordTranslate.getUkrword(), output_file);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 }
--- src/test/java/speech/SpeechTest.java
+++ src/test/java/speech/SpeechTest.java
@@ -1,41 +1,32 @@
 package speech;
 
-
 import org.junit.Test;
 import project.entities.item_implementations.words.WordTranslate;
 import project.io_data_module.CsvWordsReader;
 
 import java.io.File;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 
 public class SpeechTest
 {
 
-	private String PATH_TO_FOLDER = "c:\\dictionaries";
-	private String PATH_TO_DICTIONARY = "c:\\dictionaries\\dic.csv";
+	private String PATH_TO_FOLDER = "d:\\dictionaries";
+	private String PATH_TO_DICTIONARY = "d:\\dictionaries\\dic.csv";
 	private CsvWordsReader reader = new CsvWordsReader();
 	private Speech speech = new Speech();
 
 	/** creating mp3 list of words */
 	@Test
-    private void testSpeechingFromFile()
+    public void testSpeechingFromFile()
 	{
 		List<WordTranslate> list = reader.getItemList(PATH_TO_DICTIONARY);
 		for (WordTranslate wordTranslate : list)
 		{
+		    speech.setLanguage("en-us");
 			speech.speak(wordTranslate.getEngword());
+			speech.setLanguage("uk");
 			speech.speak(wordTranslate.getUkrword());
-			new Speech().writeToFile(PATH_TO_FOLDER, wordTranslate);
-			try
-			{
-				TimeUnit.SECONDS.sleep(2);
-			}
-			catch (InterruptedException e)
-			{
-				e.printStackTrace();
-			}
+			speech.writeToFile(PATH_TO_FOLDER, wordTranslate);
 		}
 	}
 
