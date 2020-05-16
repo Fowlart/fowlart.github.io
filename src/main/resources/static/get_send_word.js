9c08d4cd6b4e2c0f0dc9a4cfc615e4420dba100d
andrew-melnychuk <a.melnychuk93@gmail.com>
2020-05-17 02:56:55 +0300

    update XML to fetch

--- src/main/resources/static/get_send_word.js
+++ src/main/resources/static/get_send_word.js
@@ -1,100 +1,74 @@
 var score;
 
-function getWord() {
+async function getWord() {
     let data;
     let word;
     let userData;
-    let xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "/api/getWord", false);
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            data = JSON.parse(this.response); // will send object
-            userData = data["0"];
-            word = data["1"];
+    let response = await fetch('/api/getWord');
 
-            if (userData != undefined || word != undefined) {
-                let csvPicker = document.getElementById("CSV_picker");
-                if (csvPicker != undefined) csvPicker.parentNode.removeChild(csvPicker);
-                document.getElementById("profile_csv_picker_save").style.visibility = "visible";
-            }
+    if (response.ok) {
+        data = await response.json();
+        userData = data[0];
+        word = data[1];
 
-            document.getElementById("translate").innerHTML = word.ukrword;
-            document.getElementById('details').textContent = `${word.ukrword.toLocaleUpperCase()} - ${word.engword.toLocaleUpperCase()}`;
-            document.getElementById('myAudio').src = word.sound;
-
-            //   document.getElementById('user').innerHTML = userData.name;
-            score = userData.allUserPoints;
-            console.log(`score in getWord(): ${score}`);
-            let progressBar = document.getElementById('myBar');
-            progressBar.style.width = Math.ceil(userData.allUserPoints / userData.maxUserPoints * 100) + "%";
-            progressBar.innerHTML = Math.ceil(userData.allUserPoints / userData.maxUserPoints * 100) + "%";
-
+        if (userData != undefined || word != undefined) {
+            let csvPicker = document.getElementById("CSV_picker");
+            if (csvPicker != undefined) csvPicker.parentNode.removeChild(csvPicker);
+            document.getElementById("profile_csv_picker_save").style.visibility = "visible";
         }
+
+        document.getElementById("translate").innerHTML = word.ukrword;
+        document.getElementById('details').textContent = `${word.ukrword.toLocaleUpperCase()} - ${word.engword.toLocaleUpperCase()}`;
+        document.getElementById('myAudio').src = word.sound;
+
+        //   document.getElementById('user').innerHTML = userData.name;
+        score = userData.allUserPoints;
+        console.log(`score in getWord(): ${score}`);
+        let progressBar = document.getElementById('myBar');
+        progressBar.style.width = Math.ceil(userData.allUserPoints / userData.maxUserPoints * 100) + "%";
+        progressBar.innerHTML = Math.ceil(userData.allUserPoints / userData.maxUserPoints * 100) + "%";
+
+    } else {
+        let result = await response.json();
+        console.log(result.error);
     }
-    xhttp.send();
 }
 
 // Display whether user was correct or not
-function red() {
-    let wordInput = document.getElementById("WordInput");
-    wordInput.style.borderStyle = 'solid';
-    wordInput.style.borderColor = 'red';
-    wordInput.style.borderWidth = '2px';
-    wordInput.style.boxShadow = '0px 0px 5px red';
-
-}
-
-function green() {
-    let wordInput = document.getElementById("WordInput");
-    wordInput.style.borderStyle = 'solid';
-    wordInput.style.borderColor = 'green';
-    wordInput.style.borderWidth = '2px';
-    wordInput.style.boxShadow = '0px 0px 5px green';
-}
-
-function black() {
-    let wordInput = document.getElementById("WordInput");
-    wordInput.style.borderStyle = 'solid';
-    wordInput.style.borderColor = 'black';
-    wordInput.style.borderWidth = '1px';
-    wordInput.style.boxShadow = 'none';
-}
 
 function checkIfWordWasCorrect(a, b) {
+    let elem = document.getElementById('WordInput');
     if (a != b) {
-        green();
-        setTimeout(black, 700);
+        elem.classList.toggle('green');
+        setTimeout(() => elem.classList.toggle('green'), 700);
     } else {
-        red();
-        setTimeout(black, 700);
+        elem.classList.toggle('red');
+        setTimeout(() => elem.classList.toggle('red'), 700);
     }
 }
 
 //Send word
-function sendWord() {
-    let word = document.getElementById("WordInput").value;
-    let xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "/api/checkWord", false);
-    xhttp.onreadystatechange = function() {
-        console.log(`Response status: ${this.status}`);
-        if (this.readyState == 4 && this.status == 200) {
-            // everithing OK
-            document.getElementById("WordInput").value = '';
-            let scoreForCheck = score;
-            getWord();
-            setTimeout(checkIfWordWasCorrect(scoreForCheck, score), 700);
 
-        } else if (this.readyState == 4 && this.status == 400) {
-            // empty line was sent
-            red();
-            setTimeout(black, 700);
-        }
+async function sendWord() {
+    let word = document.getElementById("WordInput").value;
+    let response = await fetch('/api/checkWord', {
+        method: 'POST',
+        body: word
+    });
+
+    if (response.ok) {
+        document.getElementById("WordInput").value = '';
+        let scoreForCheck = score;
+        await getWord();
+        setTimeout(checkIfWordWasCorrect(scoreForCheck, score), 700);
+    } else {
+        let result = await response.json();
+        console.log(result.error);
     }
-    xhttp.send(word);
 }
 
 let wordInputField = document.getElementById('WordInput');
-wordInputField.addEventListener('keyup', function(e) {
+wordInputField.addEventListener('keyup', function (e) {
     if (e.keyCode === 13) {
         sendWord();
     }
