de82788d8b75d8f1c71c3b9210efbfee5668173a
Artur_Semikov <artur_semikov@epam.com>
2020-06-15 09:25:48 +0300

    15.06.2020 minor fixes

---
+++ src/main/resources/static/getLogger.js
@@ -0,0 +1,22 @@
+let container;
+let refreshButton;
+
+document.addEventListener('DOMContentLoaded', function() {
+    container = document.getElementById("loggerContainer");
+    refreshButton = document.getElementById("refresh");
+    refreshButton.addEventListener('click', getLogger);
+}, false);
+
+
+function getLogger() {
+    let logger;
+    let xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "/api/getLogger", false);
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            logger = JSON.parse(this.response);
+            container.innerHTML = logger.reduce((total, value) => total + value, "");
+        }
+    }
+    xhttp.send();
+}
