cbf0ea172d7eb3d8bbae644fe667afdb03506867
Artur_Semikov <artur_semikov@epam.com>
2020-06-15 09:23:47 +0300

    15.06.2020 minor fixes

--- src/main/resources/static/getTable.js
+++ src/main/resources/static/getTable.js
@@ -1,22 +1,33 @@
 function getTable() {
     if (document.getElementById("power").checked) {
+
         let table;
-        let xhttp = new XMLHttpRequest();
-        xhttp.open("GET", "/api/getTable", true);
-        xhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                table = JSON.parse(this.response); // will send object
-                let tableBodyLine = "";
-                for (let index in table) {
-                    let word = table[index];
-                    let row = `<tr><td>${word.id}</td><td>${word.engword}</td><td>${word.ukrword}</td><td>${word.points}</td></tr>`;
-                    tableBodyLine = tableBodyLine + row;
+        let getTablePromise = new Promise(function(resolve, reject) {
+
+            let xhttp = new XMLHttpRequest();
+            xhttp.open("GET", "/api/getTable", true);
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        table = JSON.parse(this.response); // will send object
+                        var tableBodyLine = "";
+                        for (let index in table) {
+                            let word = table[index];
+                            let row = `<tr><td>${word.id}</td><td>${word.engword}</td><td>${word.ukrword}</td><td>${word.points}</td></tr>`;
+                            tableBodyLine = tableBodyLine + row;
+                        }
+                        resolve(tableBodyLine);
+                    } else reject(new Error(">>> Error during 'GET' to '/api/getTable'."));
                 }
-                document.getElementById("tbod").innerHTML += tableBodyLine;
-                document.getElementById("thad").innerHTML += "<td>id</td><td>english word</td><td>ukranian word</td><td>points</td>";
-            };
-        }
-        xhttp.send();
+            }
+            xhttp.send();
+        });
+
+        getTablePromise.then((tableBodyLine) => {
+            document.getElementById("thad").innerHTML = "<td>id</td><td>english word</td><td>ukranian word</td><td>points</td>";
+            document.getElementById("tbod").innerHTML = tableBodyLine;
+        }, (err) => { console.error(err) });
+
     } else {
         hideTable()
     }
